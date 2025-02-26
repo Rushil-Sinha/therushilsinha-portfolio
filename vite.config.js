32929c3e9f135443401612d1ba94cfd00431fe94
Rushil Sinha <rushilsinha35@gmail.com>
2025-02-26 21:12:15 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()], 
+  base: '/therushilsinha-portfolio/', // Set base path to match your GitHub Pages repo name
 })
